f46f5b81797a2ca81e3b6d15537554bade94d8ba
hogiyanto <hogiyanto.p.lucman@gdplabs.id>
2020-07-23 13:56:31 +0700

    setup eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,32 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-typescript", "gatsby-plugin-typescript-checker"],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-typescript-checker",
+    {
+      resolve: "gatsby-plugin-prettier-eslint",
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            "**/*.{css,scss,less}",
+            "**/*.{json,json5}",
+            "**/*.{graphql}",
+            "**/*.{md,mdx}",
+            "**/*.{html}",
+            "**/*.{yaml,yml}",
+          ],
+        },
+        eslint: {
+          patterns: "**/*.{js,jsx,ts,tsx}",
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
+  ],
 };
